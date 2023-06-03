f439aa29acb27842b3aca3b1b35bdc18ec853979
Xanoster <techypankaj@gmail.com>
2023-06-03 17:09:00 +0200

    Create Transaction class

---
+++ CryptoLedger/ledger.js
@@ -0,0 +1,107 @@
+const crypto = require('crypto');
+
+class Transaction {
+    constructor(sender, receiver, amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.timestamp = Date.now();
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+class Block {
+    constructor(timestamp, transactions, previousHash = '') {
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.nonce = 0;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return crypto.createHash('sha256')
+            .update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce)
+            .digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log(`Block mined: ${this.hash}`);
+    }
+}
+
+class CryptoLedger {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
+        this.pendingTransactions = [];
+        this.miningReward = 50;
+    }
+
+    createGenesisBlock() {
+        return new Block(Date.parse('2023-06-01'), [], '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    minePendingTransactions(miningRewardAddress) {
+        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [];
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.sender === address) {
+                    balance -= trans.amount;
+                }
+                if (trans.receiver === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+module.exports = { CryptoLedger, Transaction, Block };
