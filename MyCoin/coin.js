d377ba10e347eba314828be26b22c4df9952c9ce
Xanoster <techypankaj@gmail.com>
2023-11-05 11:00:00 +0100

    Create Block class

---
+++ MyCoin/coin.js
@@ -0,0 +1,101 @@
+const crypto = require('crypto');
+
+class Block {
+    constructor(index, timestamp, transactions, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.nonce = 0;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return crypto.createHash('sha256')
+            .update(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce)
+            .digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log(`Block mined: ${this.hash}`);
+    }
+}
+
+class MyCoin {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 10;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '01/11/2023', [], '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    minePendingTransactions(miningRewardAddress) {
+        let block = new Block(
+            this.chain.length,
+            Date.now(),
+            this.pendingTransactions,
+            this.getLatestBlock().hash
+        );
+
+        block.mineBlock(this.difficulty);
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            { from: null, to: miningRewardAddress, amount: this.miningReward }
+        ];
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.from === address) {
+                    balance -= trans.amount;
+                }
+
+                if (trans.to === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = MyCoin;
